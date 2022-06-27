340b7d8ed595c987c611b7e2e327a49c9cfca5bb
dayan sauerbronn <dayansauerbronn@SCWs-MacBook-Pro.local>
2022-06-27 15:17:58 -0700

    added todo list items displaying and logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,62 @@
-import './App.css';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import { useEffect, useState } from 'react';
-import ListOfTodo from './components/ListOfTodo';
+import "./App.css";
+import firebase from "firebase/app";
+import "firebase/auth";
+import { useEffect, useState } from "react";
+import ListOfTodo from "./components/ListOfTodo";
 
 function App() {
-	const [auth, setAuth] = useState(
-		false || window.localStorage.getItem('auth') === 'true'
-	);
-	const [token, setToken] = useState('');
+  const [auth, setAuth] = useState(
+    false || window.localStorage.getItem("auth") === "true"
+  );
+  const [token, setToken] = useState("");
 
-	useEffect(() => {
-		firebase.auth().onAuthStateChanged((userCred) => {
-			if (userCred) {
-				setAuth(true);
-				window.localStorage.setItem('auth', 'true');
-				userCred.getIdToken().then((token) => {
-					setToken(token);
-				});
-			}
-		});
-	}, []);
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((userCred) => {
+      if (userCred) {
+        setAuth(true);
+        window.localStorage.setItem("auth", "true");
+        userCred.getIdToken().then((token) => {
+          setToken(token);
+        });
+      }
+    });
+  }, []);
 
-	const loginWithGoogle = () => {
-		firebase
-			.auth()
-			.signInWithPopup(new firebase.auth.GoogleAuthProvider())
-			.then((userCred) => {
-				if (userCred) {
-					setAuth(true);
-					window.localStorage.setItem('auth', 'true');
-				}
-			});
-	};
+  const loginWithGoogle = () => {
+    firebase
+      .auth()
+      .signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then((userCred) => {
+        if (userCred) {
+          setAuth(true);
+          window.localStorage.setItem("auth", "true");
+        }
+      });
+  };
 
-	return (
-		<div className="App">
-			{auth ? (
-				<ListOfTodo token={token} />
-			) : (
-				<button onClick={loginWithGoogle}>Login with Google</button>
-			)}
-		</div>
-	);
+  return (
+    <div className="App">
+      <SignOut authHandler={setAuth} />
+      {auth ? (
+        <ListOfTodo token={token} />
+      ) : (
+        <button onClick={loginWithGoogle}>Login with Google</button>
+      )}
+    </div>
+  );
+}
+
+function SignOut({ authHandler }) {
+  const handleSignOut = () => {
+    window.localStorage.setItem("auth", "false");
+    authHandler(false);
+    firebase.auth().signOut();
+  };
+  return (
+    firebase.auth().currentUser && (
+      <button onClick={() => handleSignOut()}>Logout</button>
+    )
+  );
 }
 
 export default App;
--- client/src/components/ListOfTodo.js
+++ client/src/components/ListOfTodo.js
@@ -1,24 +1,27 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 export default function ListOfTodo({ token }) {
-	useEffect(() => {
-		if (token) {
-			fetchData(token);
-		}
-	}, [token]);
+  const [data, setData] = useState();
 
-	const fetchData = async (token) => {
-		const res = await axios.get('http://localhost:5000/api/todos', {
-			headers: {
-				Authorization: 'Bearer ' + token,
-			},
-		});
-		console.log(res.data);
-	};
+  useEffect(() => {
+    if (token) {
+      fetchData(token);
+    }
+  }, [token]);
 
-	return (
-		<div>
-			<h1>List of todo</h1>
-		</div>
-	);
+  const fetchData = async (token) => {
+    const res = await axios.get("http://localhost:5000/api/todos", {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    setData(res.data);
+  };
+
+  return (
+    <div>
+      <h1>List of todo</h1>
+      {data && data.todos.map((task) => <div>{task.title}</div>)}
+    </div>
+  );
 }
